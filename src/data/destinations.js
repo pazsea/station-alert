585ffd8ff036b5edb4b0b0106268e9fde96e4cc9
paz198787 <patrick.sjoberg@student.kyh.se>
2020-04-26 23:26:07 +0200

    updated destinations

--- src/data/destinations.js
+++ src/data/destinations.js
@@ -1,47 +1,537 @@
 export default [
   {
+    name: "Akalla",
+    lat: 59.414814,
+    long: 17.912796,
+  },
+
+  {
+    name: "Alby",
+    lat: 59.239498,
+    long: 17.845332,
+  },
+  {
+    name: "Alvik",
+    lat: 59.333633,
+    long: 17.980269,
+  },
+  {
+    name: "Aspudden",
+    lat: 59.306449,
+    long: 18.001447,
+  },
+  {
+    name: "Bandhagen",
+    lat: 59.270403,
+    long: 18.049486,
+  },
+  {
+    name: "Bergshamra",
+    lat: 59.381509,
+    long: 18.036514,
+  },
+  {
+    name: "  Björkhagen ",
+    lat: 59.291114,
+    long: 18.11552,
+  },
+  {
+    name: " Blackeberg  ",
+    lat: 59.348357,
+    long: 17.882799,
+  },
+  {
+    name: " Blåsut  ",
+    lat: 59.290242,
+    long: 18.091061,
+  },
+  {
+    name: "  Bredäng ",
+    lat: 59.294835,
+    long: 17.933812,
+  },
+  {
+    name: "  Brommaplan ",
+    lat: 59.338388,
+    long: 17.93926,
+  },
+  {
+    name: " Danderyds sjukhus ",
+    lat: 59.391901,
+    long: 18.041368,
+  },
+  {
+    name: " Duvbo  ",
+    lat: 59.367891,
+    long: 17.964618,
+  },
+  {
+    name: "  Enskede gård ",
+    lat: 59.289397,
+    long: 18.070296,
+  },
+  {
+    name: "  Farsta ",
+    lat: 59.243552,
+    long: 18.093281,
+  },
+  {
+    name: " Farsta strand  ",
+    lat: 59.235012,
+    long: 18.10174,
+  },
+  {
+    name: " Fittja  ",
+    lat: 59.247462,
+    long: 17.860964,
+  },
+  {
+    name: "  Fridhemsplan ",
+    lat: 59.332203,
+    long: 18.029188,
+  },
+  {
+    name: " Fruängen  ",
+    lat: 59.285927,
+    long: 17.965005,
+  },
+  {
+    name: " Gamla stan  ",
+    lat: 59.32316,
+    long: 18.067617,
+  },
+  {
+    name: " Globen   ",
+    lat: 59.294278,
+    long: 18.077972,
+  },
+  {
+    name: " Gubbängen  ",
+    lat: 59.262879,
+    long: 18.082036,
+  },
+  {
+    name: " Gullmarsplan  ",
+    lat: 59.299114,
+    long: 18.080768,
+  },
+  {
+    name: "  Gärdet ",
+    lat: 59.347206,
+    long: 18.098791,
+  },
+  {
+    name: "  Hagsätra ",
+    lat: 59.262726,
+    long: 18.012486,
+  },
+  {
+    name: "  Hallonbergen ",
+    lat: 59.37545,
+    long: 17.969212,
+  },
+  {
+    name: " Hallunda  ",
+    lat: 59.243273,
+    long: 17.825609,
+  },
+  {
+    name: " Hammarbyhöjden  ",
+    lat: 59.294763,
+    long: 18.104554,
+  },
+  {
+    name: " Hjulsta  ",
+    lat: 59.396171,
+    long: 17.887716,
+  },
+  {
+    name: " Hornstull  ",
+    lat: 59.315834,
+    long: 18.034024,
+  },
+  {
+    name: " Husby  ",
+    lat: 59.410257,
+    long: 17.925641,
+  },
+  {
+    name: " Huvudsta  ",
+    lat: 59.349544,
+    long: 17.985698,
+  },
+
+  {
+    name: " Hägerstensåsen  ",
+    lat: 59.295572,
+    long: 17.979154,
+  },
+  {
+    name: " Hässelby gård  ",
+    lat: 59.366902,
+    long: 17.843767,
+  },
+  {
+    name: " Hässelby strand  ",
+    lat: 59.361283,
+    long: 17.832351,
+  },
+  {
+    name: "  Högdalen ",
+    lat: 59.263795,
+    long: 18.043004,
+  },
+  {
+    name: " Hökarängen  ",
+    lat: 59.257925,
+    long: 18.082494,
+  },
+  {
+    name: "  Hötorget ",
+    lat: 59.335529,
+    long: 18.063536,
+  },
+  {
+    name: "  Islandstorget ",
+    lat: 59.345858,
+    long: 17.894017,
+  },
+  {
+    name: "  Johannelund ",
+    lat: 59.367944,
+    long: 17.857467,
+  },
+  {
+    name: " Karlaplan  ",
+    lat: 59.33881,
+    long: 18.090863,
+  },
+  {
+    name: "  Kista ",
+    lat: 59.402868,
+    long: 17.942433,
+  },
+  {
+    name: " Kristineberg  ",
+    lat: 59.332815,
+    long: 18.003182,
+  },
+  {
+    name: "  Kungsträdgården ",
+    lat: 59.330783,
+    long: 18.073298,
+  },
+  {
+    name: "  Kymlinge ",
+    lat: 59.395,
+    long: 17.965,
+  },
+  {
+    name: " Kärrtorp  ",
+    lat: 59.284507,
+    long: 18.114478,
+  },
+  {
+    name: " Liljeholmen  ",
+    lat: 59.31071,
+    long: 18.023129,
+  },
+  {
+    name: " Mariatorget  ",
+    lat: 59.316958,
+    long: 18.063311,
+  },
+  {
+    name: "  Masmo ",
+    lat: 59.249682,
+    long: 17.880336,
+  },
+  {
+    name: " Medborgarplatsen  ",
+    lat: 59.314342,
+    long: 18.07355,
+  },
+  {
+    name: "Midsommarkransen   ",
+    lat: 59.301856,
+    long: 18.012037,
+  },
+  {
+    name: " Mälarhöjden  ",
+    lat: 59.300921,
+    long: 17.957283,
+  },
+  {
+    name: "   Mörby centrum",
+    lat: 59.398706,
+    long: 18.036218,
+  },
+  {
+    name: "  Norsborg ",
+    lat: 59.243794,
+    long: 17.814526,
+  },
+  {
+    name: "  Näckrosen ",
+    lat: 59.36674,
+    long: 17.98328,
+  },
+  {
+    name: " Odenplan  ",
+    lat: 59.342954,
+    long: 18.049701,
+  },
+  {
+    name: "  Rinkeby ",
+    lat: 59.388161,
+    long: 17.928778,
+  },
+  {
+    name: " Rissne  ",
+    lat: 59.375837,
+    long: 17.939961,
+  },
+  {
+    name: "  Ropsten ",
+    lat: 59.357301,
+    long: 18.102216,
+  },
+  {
+    name: "  Råcksta ",
+    lat: 59.354802,
+    long: 17.881819,
+  },
+  {
+    name: " Rådhuset  ",
+    lat: 59.330298,
+    long: 18.04207,
+  },
+  {
+    name: " Rådmansgatan  ",
+    lat: 59.340572,
+    long: 18.058771,
+  },
+  {
+    name: "  Rågsved ",
+    lat: 59.256577,
+    long: 18.028136,
+  },
+  {
+    name: "  Sandsborg ",
+    lat: 59.284785,
+    long: 18.092382,
+  },
+  {
+    name: " Sankt Eriksplan  ",
+    lat: 59.339655,
+    long: 18.036991,
+  },
+  {
+    name: "  Skanstull ",
+    lat: 59.307852,
+    long: 18.076229,
+  },
+  {
+    name: "  Skarpnäck ",
+    lat: 59.266816,
+    long: 18.133346,
+  },
+  {
+    name: "  Skogskyrkogården ",
+    lat: 59.279194,
+    long: 18.095501,
+  },
+  {
+    name: " Skärholmen  ",
+    lat: 59.277144,
+    long: 17.907007,
+  },
+  {
+    name: "Skärmarbrink  ",
+    lat: 59.295366,
+    long: 18.09044,
+  },
+  {
+    name: "Slussen   ",
+    lat: 59.319493,
+    long: 18.072327,
+  },
+  {
+    name: " Sockenplan  ",
+    lat: 59.283302,
+    long: 18.070592,
+  },
+  {
+    name: " Solna centrum  ",
+    lat: 59.358856,
+    long: 17.998975,
+  },
+  {
+    name: "  Solna strand ",
+    lat: 59.354191,
+    long: 17.973985,
+  },
+  {
+    name: " Stadion  ",
+    lat: 59.342963,
+    long: 18.081703,
+  },
+  {
+    name: "Stadshagen   ",
+    lat: 59.336959,
+    long: 18.017322,
+  },
+  {
+    name: " Stora mossen  ",
+    lat: 59.334532,
+    long: 17.966192,
+  },
+  {
+    name: " Stureby  ",
+    lat: 59.2746,
+    long: 18.055625,
+  },
+  {
+    name: "  Sundbybergs centrum ",
+    lat: 59.360897,
+    long: 17.972214,
+  },
+  {
+    name: "   Svedmyra",
+    lat: 59.277639,
+    long: 18.06723,
+  },
+  {
+    name: "  Sätra ",
+    lat: 59.284983,
+    long: 17.921371,
+  },
+  {
+    name: " Tallkrogen  ",
+    lat: 59.27114,
+    long: 18.085326,
+  },
+
+  {
+    name: "  T-Centralen ",
+    lat: 59.330945,
+    long: 18.059266,
+  },
+  {
+    name: " Tekniska högskolan  ",
+    lat: 59.345822,
+    long: 18.071716,
+  },
+  {
+    name: " Telefonplan  ",
+    lat: 59.298323,
+    long: 17.997231,
+  },
+  {
+    name: "Thorildsplan   ",
+    lat: 59.331817,
+    long: 18.015434,
+  },
+  {
+    name: "  Universitetet ",
+    lat: 59.365571,
+    long: 18.054888,
+  },
+  {
+    name: " Vårberg  ",
+    lat: 59.275931,
+    long: 17.890161,
+  },
+
+  {
+    name: " Vårby gård  ",
+    lat: 59.264613,
+    long: 17.884399,
+  },
+  {
+    name: " Vällingby  ",
+    lat: 59.363252,
+    long: 17.872066,
+  },
+  {
+    name: " Västertorp  ",
+    lat: 59.291383,
+    long: 17.966668,
+  },
+  {
+    name: "  Västra skogen ",
+    lat: 59.347476,
+    long: 18.003991,
+  },
+  {
+    name: " Zinkensdamm  ",
+    lat: 59.317776,
+    long: 18.050151,
+  },
+  {
+    name: " Åkeshov  ",
+    lat: 59.342038,
+    long: 17.924904,
+  },
+  {
+    name: " Ängbyplan  ",
+    lat: 59.341885,
+    long: 17.907052,
+  },
+  {
+    name: " Örnsberg  ",
+    lat: 59.305532,
+    long: 17.989204,
+  },
+  {
+    name: " Östermalmstorg  ",
+    lat: 59.334972,
+    long: 18.07408,
+  },
+
+  {
     name: "Abrahamsberg",
     lat: 59.336516,
-    long: 17.9521016
+    long: 17.9521016,
   },
   {
     name: "Axelsberg",
     lat: 59.304359,
-    long: 17.974909
+    long: 17.974909,
   },
   {
     name: "Bagarmossen",
     lat: 59.276318,
-    long: 18.131311
+    long: 18.131311,
   },
   {
     name: "Bergshamra",
     lat: 59.379409,
-    long: 18.038027
+    long: 18.038027,
   },
   {
     name: "Knivsta",
     lat: 59.725739,
-    long: 17.78797
+    long: 17.78797,
   },
   {
     name: "Tensta",
     lat: 59.395,
-    long: 17.9047
+    long: 17.9047,
   },
   {
     name: "Karlaplan",
     lat: 59.340292,
-    long: 18.09386
+    long: 18.09386,
   },
   {
     name: "Kärrtorp",
     lat: 59.284031,
-    long: 18.11472
+    long: 18.11472,
   },
   {
     name: "Kungsträdgården",
     lat: 59.330672,
-    long: 18.07384
-  }
+    long: 18.07384,
+  },
 ];
